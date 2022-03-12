05576bbe4563be42c946ee3e63bead796319e426
David Ng <david.ng.dev@gmail.com>
2022-03-12 22:52:45 +0000

    initial commit

--- karma.conf.js
+++ karma.conf.js
@@ -3,42 +3,39 @@
 
 module.exports = function (config) {
   config.set({
+    autoWatch: true,
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
+    browsers: ['Chrome'],
     client: {
+      clearContext: false,
       jasmine: {
         // you can add configuration options for Jasmine here
         // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      }, // leave Jasmine Spec Runner output visible in browser
     },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
-    },
+    colors: true,
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/angualr-complete-guide'),
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
       subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
     },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    jasmineHtmlReporter: {
+      suppressAll: true, // removes the duplicated traces
+    },
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    port: 9876,
+    reporters: ['progress', 'kjhtml'],
+    restartOnFileChange: true,
     singleRun: false,
-    restartOnFileChange: true
   });
 };
