f4a67a2b8f2de1185520749977725898223e5e4b
David Ng <david.ng.dev@gmail.com>
2022-03-13 16:34:52 +0000

    fix: ng lint not working

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,23 @@
 module.exports = {
   extends: ['@busybox'],
+  overrides: [
+    {
+      extends: [
+        'plugin:@angular-eslint/recommended',
+        // This is required if you use inline templates in Components
+        'plugin:@angular-eslint/template/process-inline-templates',
+      ],
+      files: ['*.ts'],
+      parserOptions: {
+        createDefaultProgram: true,
+        project: ['tsconfig.app.json', 'tsconfig.spec.json'],
+      },
+    },
+    {
+      extends: ['plugin:@angular-eslint/template/recommended'],
+      files: ['*.html'],
+    },
+  ],
   rules: {
     'no-console': 'off',
   },
